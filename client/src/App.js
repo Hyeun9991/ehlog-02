272bad3ec65a4bc66bdad145d70feac33f4c802b
Hyeun9991 <hyeun9991@gmail.com>
2023-07-05 17:34:10 +0900

    React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import HomePage from './pages/home/HomePage';
+import AboutPage from './pages/about/AboutPage';
+import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
